bd433a789c1e46ca70cbc3b1e2ab9da84499bd4b
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-05-23 21:42:13 -0400

    Create quantitattive_methy_express.R

---
+++ Methylation_Expression/quantitattive_methy_express.R
@@ -0,0 +1,118 @@
+---
+title: "Quantitative_methy_express"
+author: "Yibing Zeng"
+date: "2023-05-23"
+output: html_document
+---
+```{r}
+library(tidyverse)
+```
+#Match the pangene and gene name to create the pan matrix for the CG/CHG methylation levels
+1.Read all core  gene files
+```{r}
+coregene_path = "/Users/x/Desktop/Data/core/"
+core_file <- list.files(coregene_path)
+for (i in 1:length(core_file)) {
+  assign(core_file[i],read_table(paste0(coregene_path,core_file[i]), col_names = 
+             c("chr","start","end","strand","gene","pan","copy","duplicate"))[,5:8])
+}
+```
+2. Read all methylation level files
+```{r}
+methy_path = "/Users/x/Desktop/Data/methylation/cgchgmtr/"
+methy_file = list.files(methy_path)
+methy_names = gsub("core","methy",core_file)
+for (i in 1:length(methy_file)) {
+  assign(methy_names[i],
+         read.table(paste0(methy_path,methy_file[i]),
+           col.names = c("chr","start","end","strand","mCG","cCG","mCHG","cCHG","gene","epiallele")))
+}
+```
+3. Use merge to put together pangene names and gene names
+```{r}
+pan_methy_names = gsub("core","pan_methy",core_file)
+for (i in 1:length(pan_methy_names)) {
+  assign(pan_methy_names[i],
+         merge(get(core_file[i]),
+               get(methy_names[i]),
+               by = "gene")
+         )
+}
+```
+4. Mask the ambiguous and teM gene mCG as NA and extract pan and mCG values to
+prepare the mCG pan gene matrix
+```{r}
+mCG_list <- gsub("core","mCG",core_file)
+for (i in 1:length(mCG_list)) {
+  assign(mCG_list[i],
+       select(filter(get(pan_methy_names[i]),
+                duplicate=="N"),
+              c("pan","mCG","epiallele")
+              )
+        )
+}
+```
+mask the ambiguous and teM genes' mCG values
+```{r}
+B73.mCG$mCG[B73.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+B97.mCG$mCG[B97.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML103.mCG$mCG[CML103.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML228.mCG$mCG[CML228.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML247.mCG$mCG[CML247.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML277.mCG$mCG[CML277.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML322.mCG$mCG[CML322.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML333.mCG$mCG[CML333.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML52.mCG$mCG[CML52.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+CML69.mCG$mCG[CML69.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+HP301.mCG$mCG[HP301.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+IL14H.mCG$mCG[IL14H.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Ki11.mCG$mCG[Ki11.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Ki3.mCG$mCG[Ki3.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Ky21.mCG$mCG[Ky21.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+M162W.mCG$mCG[M162W.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+M37W.mCG$mCG[M37W.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Mo18W.mCG$mCG[Mo18W.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+MS71.mCG$mCG[MS71.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+NC350.mCG$mCG[NC350.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+NC358.mCG$mCG[NC358.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Oh43.mCG$mCG[Oh43.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Oh7B.mCG$mCG[Oh7B.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+P39.mCG$mCG[P39.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Tx303.mCG$mCG[Tx303.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+Tzi8.mCG$mCG[Tzi8.mCG$epiallele %in% c("ambiguous","teM")] <- NA
+```
+#get rid of the epiallele column
+```{r}
+for (i in 1:length(mCG_list)) {
+  assign(mCG_list[i],
+       select(get(pan_methy_names[i]),
+              c("pan","mCG")
+              )
+        )
+}
+```
+aggregate the multiple copy genes
+```{r}
+pre_methy_list <- gsub("methy","pre",methy_names)
+for (i in 1:length(pre_methy_list)) {
+  assign(pre_methy_list[i],
+         aggregate(mCG~pan,
+                data = get(mCG_list[i]),
+                FUN = paste0%>%unlist)
+         )
+}
+```
+
+```{r}
+pan_mCG <- merge(get(pre_methy_list[1]),
+                 get(pre_methy_list[2]),
+                 by = "pan",
+                 all = T)
+for (i in 3:length(pre_methy_list)) {
+  pan_mCG <- merge(pan_mCG,
+                   get(pre_methy_list[i]),
+                   by="pan",
+                   all=T)
+}
+names(pan_mCG) <- c("pan",gsub(".core","",core_file))
+```
