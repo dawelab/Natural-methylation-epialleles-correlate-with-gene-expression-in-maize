d8d5b34eaf371f610a3be107f90cb8aeff0b2280
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-06-13 00:10:01 -0400

    Update element_length.R

--- Structural feautures/element_length.R
+++ Structural feautures/element_length.R
@@ -5,14 +5,16 @@ B73_exon_intron_CDS_UTR <- read.csv("B73.intron.exon.CDS.UTR.txt",
                                     header = F,
                                     col.names = c("element","length","gene"))
 
+
 B73_TE <- read.csv("/Users/x/Desktop/R/element_length/Zm-B73-REFERENCE-NAM-5.0.intron_in_TE.txt",
                    sep = "\t",
                    header = F,
                    col.names = c("element","length","gene"))
+
 #For different element, perform different operation 
 #Get the individual UTR length for different genes
 UTR_length <- B73_exon_intron_CDS_UTR %>% filter(element=="five_prime_UTR" |
-                                                 element=="three_prime_UTR") 
+                                                 element=="three_prime_UTR")
 #Add up the element length for exon and intron
 B73_exon <- B73_exon_intron_CDS_UTR %>% filter(element == "exon") %>%
               group_by(gene) %>% summarise(sum(length)) %>% as.data.frame() 
@@ -39,6 +41,7 @@ TE_length <- data.frame(element="TE",
                         length=B73_TE_mean$length,
                         gene = B73_TE_mean$gene)
 
+
 df_length <- rbind(UTR_length,exon_length,intron_length,CDS_length,TE_length)
 
 #Read in the core gene and epiallele data set for the subsetting
@@ -54,15 +57,34 @@ B73.epiallele <- read.csv("/Users/x/Desktop/Data/methylation/cgchgmtr/Zm-B73-REF
 B73.core.epi <- merge(B73.core,B73.epiallele,by="gene",all.x = T)
 
 #Subset the core gene and add the epiallele column
-B73.core.epi.length <- merge(df_length,B73.core.epi, by="gene",all.y = T) %>% unique()
-B73.core.epi.length$element[B73.core.epi.length$element=="five_prime_UTR" | B73.core.epi.length$element=="three_prime_UTR"] <- "UTR"
-B73.core.epi.length <- B73.core.epi.length[B73.core.epi.length$epiallele!="ambiguous",]
-B73.core.epi.length$epiallele = factor(B73.core.epi.length$epiallele,levels = c("UM","gbM","teM"))
-B73.core.epi.length$element = factor(B73.core.epi.length$element,
-                                     levels = c("UTR","TE","CDS","exon","intron"))
+TE_core = merge(TE_length,B73.core.epi,by="gene",all.y = T)
+TE_core$element[is.na(TE_core$element)] <- "TE"
+TE_core$length[is.na(TE_core$length)] <- 0
 
-#Plotting
-ggplot(B73.core.epi.length,aes(x=epiallele,y=length,fill=element)) +
+UTR_core = merge(UTR_length,B73.core.epi,by="gene",all.y = T)
+UTR_core$element[is.na(UTR_core$element)] <- "UTR"
+UTR_core$length[is.na(UTR_core$length)] <- 0
+
+intron_core = merge(intron_length,B73.core.epi,by="gene",all.y = T)
+intron_core$element[is.na(intron_core$element)] <- "intron"
+intron_core$length[is.na(intron_core$length)] <- 0
+
+exon_core = merge(exon_length,B73.core.epi,by="gene",all.y = T)
+exon_core$element[is.na(exon_core$element)] <- "exon"
+exon_core$length[is.na(exon_core$length)] <- 0
+
+CDS_core = merge(CDS_length,B73.core.epi,by="gene",all.y = T)
+CDS_core$element[is.na(CDS_core$element)] <- "CDS"
+CDS_core$length[is.na(CDS_core$length)] <- 0
+
+df_length = rbind(CDS_core,exon_core,intron_core,TE_core,UTR_core)
+
+df_length$element[df_length$element =="five_prime_UTR"| df_length$element =="three_prime_UTR"] <- "UTR"
+df_length <- df_length[df_length$epiallele!="ambiguous",]
+df_length$epiallele = factor(df_length$epiallele,levels = c("UM","gbM","teM"))
+df_length$element = factor(df_length$element,
+                                     levels = c("UTR","TE","CDS","exon","intron"))
+ggplot(df_length,aes(x=epiallele,y=length,fill=element)) +
   geom_boxplot(outlier.size=.01,
                width = 0.5,
                position=position_dodge(width = 0.7)) +
@@ -72,4 +94,4 @@ ggplot(B73.core.epi.length,aes(x=epiallele,y=length,fill=element)) +
   stat_boxplot(geom ='errorbar',
                width = 0.5,
                position=position_dodge(width = 0.7)) + 
-  scale_y_continuous(breaks = c(0,2000,4000,6000,8000)) + ylab("") 
+  scale_y_continuous(breaks = c(0,2000,4000,6000,8000)) + ylab("")
