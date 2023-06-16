d18060565e5d829ebe24fe83d92c4ef4f4ec265b
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-06-16 10:57:23 -0400

    Update Match_panID_geneID.py

--- patterns of gene methylation/Match_panID_geneID.py
+++ patterns of gene methylation/Match_panID_geneID.py
@@ -1,22 +1,20 @@
-#import os
+import os
 import pandas as pd
-from pandas import DataFrame
-import numpy as np
+os.chdir("/Users/x/Desktop/Data/core_gene")
+os.getcwd()
 from pandas.core.frame import DataFrame
-data = pd.read_csv("/Users/x/Desktop/Data/pan_gene/pan_gene_matrix_v3_cyverse_ID.csv")
-#os. getcwd() 
+data = pd.read_csv("/Users/x/Desktop/Data/pan_gene/pan_gene_matrix_v3_cyverse.csv")
 pan = data['Pan_gene_ID']
-data_core = data[data['class']=="Core Gene"]
-df = data
-#df['pan'] = data_core['Pan_gene_ID']
-pan = data_core['Pan_gene_ID']
+pangeneclass = data['class']
+list = data.columns.values.tolist()
 def core(NAM,name):
-    df_pan = df.iloc[:,NAM]
+    df_pan = data.iloc[:,NAM]
     new = df_pan.str.split(";", n = 200, expand = True)
-    df2 = DataFrame(new)
-    df2['pan'] = pan
-    df3 = DataFrame(df2)
-    return(df3.to_csv(name,na_rep='NA',index = None))
-list = df.columns.values.tolist()
-for i in range(0,26):
-    core(i,list[i] + ".core.txt")
+    df = DataFrame(new)
+    df['pan'] = pan
+    df['pangeneclass'] = pangeneclass
+    df = DataFrame(df)
+    return(df.to_csv(name,na_rep='NA',index = None))
+list = data.columns.values.tolist()
+for i in range(3,29):
+    core(i,list[i] + ".class.txt")
