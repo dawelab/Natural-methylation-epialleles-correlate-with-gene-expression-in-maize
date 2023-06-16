fa0ba0a7e7c2eb0a09c56e7397e6ad24b50084e2
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-06-16 11:46:54 -0400

    Update core_df2core_list.R

--- patterns of gene methylation/core_df2core_list.R
+++ patterns of gene methylation/core_df2core_list.R
@@ -1,24 +1,21 @@
-setwd("/Users/x/Desktop/Data/pan_gene/version3")
+setwd("/Users/x/Desktop/Data/core_gene")
 melt = function(df,NAM){
   gene = NULL
-  for (i in 1:(dim(df)[2]-3)){
+  for (i in 1:(dim(df)[2]-2)){
     temp = as.character(df[,i])
     gene = c(gene,temp)
   }
   
-  copy = rep(df$copy,dim(df)[2]-3)
-  pan = rep(df$pan,dim(df)[2]-3)
-  class = rep(df$class,dim(df)[2]-3)
+  pan = rep(df$pan,dim(df)[2]-2)
+  class = rep(df$pangeneclass,dim(df)[2]-2)
   
-  df2 = data.frame(gene,pan,copy,class)
-  df2$duplicate = "Y"
-  df2$duplicate[df2$copy==1] <- "N"
+  df2 = data.frame(gene,pan,class)
   df3 = df2[complete.cases(df2),]
-  
-  return(write.csv(df3,NAM, sep = "\t", quote = F, row.names = F))
+  return(write.csv(df3,paste0(NAM,".class"), sep = "\t", quote = F, row.names = F))
 }
-samples = list.files("/Users/x/Desktop/Data/pan_gene/version3")[-24]
+samples = list.files()
+sample_name <- gsub(".class.txt","",samples)
 for (i in 1:26){
-  data = read.table("B97.core",sep = ",", header = T)
-  melt(data,"B97")
+  data = read.csv(samples[i],sep = ",", header = T)
+  melt(data,sample_name[i])
 }
