ec5d00f7e9d008951f78fed6636b2fe2542cd208
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-06-26 16:06:54 -0400

    Update core_df2core_list.R

--- patterns of gene methylation/core_df2core_list.R
+++ patterns of gene methylation/core_df2core_list.R
@@ -1,19 +1,22 @@
 setwd("/Users/x/Desktop/Data/core_gene")
 melt = function(df,NAM){
+  
+  #melt all the gene into list and remove NA and organize into dataframe
   gene = NULL
-  for (i in 1:(dim(df)[2]-2)){
+  for (i in 1:(dim(df)[2]-3)){
     temp = as.character(df[,i])
     gene = c(gene,temp)
   }
   
-  pan = rep(df$pan,dim(df)[2]-2)
-  class = rep(df$pangeneclass,dim(df)[2]-2)
+  pan = rep(df$pan,dim(df)[2]-3)
+  class = rep(df$pangeneclass,dim(df)[2]-3)
+  copy = rep(df$copy,dim(df)[2]-3)
   
-  df2 = data.frame(gene,pan,class)
+  df2 = data.frame(gene,pan,class,copy)
   df3 = df2[complete.cases(df2),]
   return(write.csv(df3,paste0(NAM,".class"), sep = "\t", quote = F, row.names = F))
 }
-samples = list.files()
+samples = list.files()[-11]
 sample_name <- gsub(".class.txt","",samples)
 for (i in 1:26){
   data = read.csv(samples[i],sep = ",", header = T)
