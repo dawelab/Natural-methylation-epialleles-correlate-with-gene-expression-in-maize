b81bbd1f580682bf099933fea6562622f6ec482c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-07-03 15:27:31 -0400

    Update B73_expression.R

--- Structural feautures/B73_expression.R
+++ Structural feautures/B73_expression.R
@@ -1,26 +1,33 @@
 #Read data, there are 10 (Hufford et al.) + 4 Tissues (Warman et al)
 library("tidyverse")
+library(cowplot)
+
 Hufford_data = read.table("/Users/x/Desktop/Data/expression/TPM/B73.tpm.txt",
                           header = T)
 Hufford_data$status <- "tissue"
 Hufford_data$status[rowSums(as.matrix(Hufford_data[,5:14])>=1)==10] <- "expressed"
 Hufford_data$status[rowSums(as.matrix(Hufford_data[,5:14])<1)==10] <- "silenced"
 table(Hufford_data$status)
+#The abbrevation of MP, MS, SC & TP are Mature pollen, Microspore, Sperm Cell, Tassel Primodial,respectively.
+#Warman_MP = read.table("/Users/x/Desktop/Data/expression/MP.merged.bam.count",header = T)
+#Warman_MS = read.table("/Users/x/Desktop/Data/expression/MS.merged.bam.count",header = T)
+#Warman_SC = read.table("/Users/x/Desktop/Data/expression/SC.merged.bam.count",header = T)
+#Warman_TP = read.table("/Users/x/Desktop/Data/expression/TP.merged.bam.count",header = T)
+
 #Import and merge the core gene set of B73
 B73.core<- read.csv("/Users/x/Desktop/Data/core_gene/B73.class.txt",
                     sep = ",",
-                    header = T) %>% filter(class=="Core Gene")
+                    header = T) %>% filter(class=="Core Gene") %>% 
+  select(c("gene","class")) %>% unique()
 
 B73.epiallele <- read.csv("/Users/x/Desktop/Data/methylation/cgchgmtr/Zm-B73-REFERENCE-NAM-5.0.1.canon.gene.gene.mtr.ID.type.txt",
                           sep = "\t",
                           header = F,
                           col.names = c("chr","start","end","strand","mCG","cCG","mCHG","cCHG","gene","epiallele"))[,9:10]
-B73.core.epi <- merge(B73.core,B73.epiallele,by="gene",all.x = T)
+B73.core.epi <- merge(B73.core,B73.epiallele,by="gene",all.x = T)[,-2] %>% filter(epiallele!="ambiguous") %>% unique()
 
-B73_core_epiallele = merge(B73.core,B73.epiallele,by="gene",all.x = T)
-B73_core_epiallele = B73_core_epiallele[B73_core_epiallele$epiallele!="ambiguous",]
-temp = merge(Hufford_data,B73_core_epiallele,by="gene") 
-table(temp$status,temp$epiallele)
+temp = merge(Hufford_data,B73.core.epi,by="gene",all.y = T) 
+table(temp$status,temp$epiallele) 
 df_Hufford = data.frame(tpm = unlist(temp[,5:14],use.names = F),
                         tissue = rep(names(temp)[5:14],each=length(temp$gene)),
                         epiallele = rep(temp$epiallele,10))
@@ -32,58 +39,64 @@ df_Hufford$tpm = df_Hufford$tpm +1
 df_Hufford$epiallele <- factor(df_Hufford$epiallele,levels = c("UM","gbM","teM"))
 df_Hufford$tissue <- factor(df_Hufford$tissue,levels = c("tip","middle","base","root","shoot","ear","anther","tassel","endosperm","embryo"))
 #pdf("Fig2F_10tissue_notext.pdf",width =8 ,height = 4)
-F2F = ggplot(df_Hufford,aes(x=tissue,y=log10(tpm),fill=epiallele)) +
+(F2F = ggplot(df_Hufford,aes(x=tissue,y=log10(tpm),fill=epiallele)) +
   geom_boxplot(outlier.size =0.01)+
   theme_bw() + ylab("log10(tpm+1)") +
   coord_cartesian(ylim=c(0,5)) +
-  stat_boxplot(geom ='errorbar',position=position_dodge())# +
-  theme(text = element_blank())
+  stat_boxplot(geom ='errorbar',position=position_dodge()) +
+  theme(text = element_blank()))
 
 #expression pattern
 
 df_pattern = data.frame(methylation = rep(c("UM","gbM","teM"),3),
                         pattern = rep(c("expressed","silenced","tissue"),each=3),
                         count =c(unlist(table(temp$epiallele,temp$status)[c(3,1,2),],use.names = F)) )
-df_pattern$methylation = factor(df_pattern$methylation,levels = c("UM","gbM","teM"))
+df_pattern$methylation = factor(df_pattern$methylation,levels = c("teM","gbM","UM"))
+df_pattern$pattern = factor(df_pattern$pattern,
+                            levels = c("silenced","expressed","tissue"))
+
 df_patternUM = df_pattern[df_pattern$methylation=="UM",]
 df_patternUM$persent = df_patternUM$count/12666
 df_patterngbM = df_pattern[df_pattern$methylation=="gbM",]
 df_patterngbM$persent = df_patterngbM$count/7541
 df_patternteM = df_pattern[df_pattern$methylation=="teM",]
 df_patternteM$persent = df_patternteM$count/710
-UM_pattern = ggplot(df_patternUM,aes(x=methylation,y=count,fill=pattern)) +
+(UM_pattern = ggplot(df_patternUM,aes(x=methylation,y=count,fill=pattern)) +
   geom_bar(stat = "identity",position = "stack", width = 1) +
   coord_polar(theta = "y") +
+    scale_fill_manual(values=c("#619CFF","#00BA38","#F8766D")) +
   facet_grid(~methylation) +
   theme_bw() + theme(legend.position = "none",
                      axis.text = element_blank(),
                      axis.ticks = element_blank(),
                      panel.grid.major=element_blank(),
                      panel.grid.minor=element_blank()) +
-  xlab("") +ylab("")
+  xlab("") +ylab(""))
 
   
-gbM_pattern = ggplot(df_patterngbM,aes(x=methylation,y=count,fill=pattern)) +
+(gbM_pattern = ggplot(df_patterngbM,aes(x=methylation,y=count,fill=pattern)) +
   geom_bar(stat = "identity",position = "stack", width = 1) +
   coord_polar(theta = "y") +
+    scale_fill_manual(values=c("#619CFF","#00BA38","#F8766D")) +
   facet_grid(~methylation) +
   theme_bw() + theme(legend.position = "none",
                      axis.text = element_blank(),
                      axis.ticks = element_blank(),
                      panel.grid.major=element_blank(),
                      panel.grid.minor=element_blank()) +
-  xlab("") +ylab("")
+  xlab("") +ylab(""))
 
-teM_pattern = ggplot(df_patternteM,aes(x=methylation,y=count,fill=pattern)) +
+(teM_pattern = ggplot(df_patternteM,aes(x=methylation,y=count,fill=pattern)) +
   geom_bar(stat = "identity",position = "stack", width = 1) +
   coord_polar(theta = "y") +
+    scale_fill_manual(values=c("#619CFF","#00BA38","#F8766D")) +
   facet_grid(~methylation) +
   theme_bw() + theme(legend.position = "none",
                      axis.text = element_blank(),
                      axis.ticks = element_blank(),
                      panel.grid.major=element_blank(),
                      panel.grid.minor=element_blank()) +
-  xlab("") +ylab("")
+  xlab("") +ylab(""))
 
 teM_pattern2 = ggplot(df_patternteM,aes(x=methylation,y=count,fill=pattern)) +
   geom_bar(stat = "identity",position = "stack", width = 1) +
