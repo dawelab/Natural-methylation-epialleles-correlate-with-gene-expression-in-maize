067de1cc329d30633d27aacbaed34c2cbf9c6349
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-08-14 14:34:47 -0400

    Update Core_gene_expression_pattern.R

--- Structural feautures/Core_gene_expression_pattern.R
+++ Structural feautures/Core_gene_expression_pattern.R
@@ -2,30 +2,17 @@ library(tidyverse)
 library(cowplot)
 
 #Read in the core gene and epiallele data set for the subsetting
-B73.core<- read.csv("/Users/x/Desktop/Data/core/B73.core",
-                    sep = "\t",
-                    header = F,
-                    col.names =  c("chr","start","end","strand","gene","pan","copy","duplicate"))[,5:6]
-
-B73.epiallele <- read.csv("/Users/x/Desktop/Data/methylation/cgchgmtr/Zm-B73-REFERENCE-NAM-5.0.1.canon.gene.gene.mtr.ID.type.txt",
-                          sep = "\t",
-                          header = F,
-                          col.names = c("chr","start","end","strand","mCG","cCG","mCHG","cCHG","gene","epiallele"))
-B73.core.epi <- merge(B73.core,B73.epiallele,by="gene",all.x = T)
-
-
-Hufford_data = read.table("/Users/x/Desktop/Data/expression/TPM/B73.tpm.txt",
-                          header = T)
-Hufford_data$status <- "tissue"
-Hufford_data$status[rowSums(as.matrix(Hufford_data[,5:14])>=1)==10] <- "expressed"
-Hufford_data$status[rowSums(as.matrix(Hufford_data[,5:14])<1)==10] <- "silenced"
-table(Hufford_data$status)
-df_Hufford_data_core  = merge(B73.core.epi,Hufford_data,by="gene",all.x = T)
-table(df_Hufford_data_core$epiallele,df_Hufford_data_core$status)
-df_Hufford_data_core <- df_Hufford_data_core[df_Hufford_data_core$epiallele!="ambiguous",]
+B73_expr <- read.csv("https://raw.githubusercontent.com/dawelab/Natural-methylation-epialleles-correlate-with-gene-expression-in-maize/main/Data/B73.all.csv") %>% filter(class == "Core Gene" & epiallele != "ambiguous")
+B73_expr[B73_expr$gene == "Zm00001eb007510",] 
+B73_expr <- B73_expr[,c(10,14:23)]
+B73_expr$status <- "tissue"
+B73_expr$status[rowSums(as.matrix(B73_expr[,2:11])>=1)==10] <- "expressed"
+B73_expr$status[rowSums(as.matrix(B73_expr[,2:11])<1)==10] <- "silenced"
+#There is one gene that are duplicated in the provided pangene table, we subtract that 1 fromt the following table
+table(B73_expr$epiallele,B73_expr$status)
 df_2E = data.frame(epiallele=rep(c("gbM","teM","UM"),3),
                    status = rep(c("constitutive","silent","tissue-specific"),each=3), 
-                   count = c(table(df_Hufford_data_core$epiallele,df_Hufford_data_core$status)))
+                   count = c(table(B73_expr$epiallele,B73_expr$status)))
 
 df_2E$epiallele = factor(df_2E$epiallele,levels = c("UM","gbM","teM"))
 df_2E$status = factor(df_2E$status,levels = c("constitutive","silent","tissue-specific"))
@@ -62,11 +49,11 @@ F2E3 = ggplot(df_2E[df_2E$epiallele=="teM",],aes(x=epiallele,y=count,fill=status
   xlab("") +ylab("")
 F2E = plot_grid(F2E1,F2E2,F2E3,nrow = 1)
 
-df_TPM_core = data.frame(tissue=rep(names(df_Hufford_data_core)[6:15],each=dim(df_Hufford_data_core)[1]),
-                         TPM = unlist(c(df_Hufford_data_core[,6:15])),
-                         epiallele = rep(df_Hufford_data_core$epiallele,10))
+df_TPM_core = data.frame(tissue=rep(names(B73_expr)[2:11],each=dim(B73_expr)[1]),
+                         TPM = unlist(c(B73_expr[,2:11])),
+                         epiallele = rep(B73_expr$epiallele,10))
 df_TPM_core$epiallele = factor(df_TPM_core$epiallele,levels = c("UM","gbM","teM"))
-df_TPM_core$tissue = factor(df_TPM_core$tissue,levels = names(df_Hufford_data_core)[c(11,12,13,9,8,10,15,14,6,7)])
+df_TPM_core$tissue = factor(df_TPM_core$tissue,levels = names(B73_expr)[c(9,8,7,4,5,10,6,11,3,2)])
 F2F = ggplot(df_TPM_core,aes(x=tissue,y=log10(TPM+1),fill=epiallele)) +
   geom_boxplot(outlier.size =0.01)+
   theme_bw() + ylab("log10(tpm+1)") +
