678996fb67948b26e514fddf34fb40d29b752ac1
Yibing <48507231+YibingZeng@users.noreply.github.com>
2023-08-14 14:40:40 -0400

    Update UTR_epiallele.R

--- Structural feautures/UTR_epiallele.R
+++ Structural feautures/UTR_epiallele.R
@@ -1,18 +1,8 @@
 library(tidyverse)
 library(extrafont)
 #Read in the core gene and epiallele data set for the subsetting
-B73.core<- read.table("/Users/x/Desktop/Data/core_gene/B73.class.txt",
-                      header = T, sep = ",")
-
-B73.epiallele <- read.csv("/Users/x/Desktop/Data/methylation/cgchgmtr/Zm-B73-REFERENCE-NAM-5.0.1.canon.gene.gene.mtr.ID.type.txt",
-                          sep = "\t",
-                          header = F,
-                          col.names = c("chr","start","end","strand","mCG","cCG","mCHG","cCHG","gene","epiallele"))
-B73.core.epi <- merge(B73.core[B73.core$class=="Core Gene",],
-                       B73.epiallele,by="gene",
-                       all.x = T
-                       )
-table(B73.core.epi$epiallele)
+B73.core = read.csv("https://raw.githubusercontent.com/dawelab/Natural-methylation-epialleles-correlate-with-gene-expression-in-maize/main/Data/B73.all.csv")
+table(B73.core$epiallele)
 B73_UTR <- read.csv("/Users/x/Desktop/R/element_length/B73_CDS_exon_intron_UTR.txt",
                     sep = "\t", header = F)
 names(B73_UTR) <- c("element","length","gene")
@@ -30,9 +20,9 @@ element_length_UTR <- element_length_UTR %>% select(c("gene","epiallele","length
 table(element_length_UTR$epiallele)
 
 (UTR_count = table(element_length_UTR$length>0,element_length_UTR$epiallele))
-df_UTR = data.frame(proportion = c(11940/(11940+76),7511/(7511+29),291/(419+291)),
+df_UTR = data.frame(proportion = c(11940/(11940+726),7511/(7511+29),291/(419+291)),
                     methylation = c("UM","gbM","teM"))
 df_UTR$methylation = factor(df_UTR$methylation,levels = c("UM","gbM","teM"))
 (F2C=ggplot(df_UTR,aes(x=methylation,y = proportion)) +
-  geom_bar(stat = "identity") +
-  theme_bw() )
+    geom_bar(stat = "identity") +
+    theme_bw() )
